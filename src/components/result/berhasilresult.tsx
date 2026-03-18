75c61ba6f5f169e11a63dae68ceae88f3c5290a7
kevinalvarel <alvarelkevin12@gmail.com>
2026-03-18 20:25:58 +0700

    fix: update button text in BerhasilResult component to be more concise

--- src/components/result/berhasilresult.tsx
+++ src/components/result/berhasilresult.tsx
@@ -45,7 +45,7 @@ export default function BerhasilResult({
               onClick={onClose}
               className="bg-green-400 text-black font-black text-lg px-12 py-3 border-4 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] hover:translate-x-[2px] hover:translate-y-[2px] transition-all uppercase"
             >
-              Kembali ke Dashboard
+              Kembali
             </button>
           </div>
         </div>
